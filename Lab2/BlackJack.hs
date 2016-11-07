5fd84ce65244749b07f1d463d13a034dbbbc97fe
johannes <johannes.o.andersson@gmail.com>
2016-11-07 16:01:18 +0100

    Lab 2 rev 1

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -4,7 +4,7 @@ import RunGame
 
 
 
-{-Task 3.0-}
+{-Task 3.1-}
 {-
 size hand2
   = size (Add (Card (Numeric 2) Hearts)
@@ -16,7 +16,7 @@ size hand2
 -}
 
 
-{-Task 3.1-}
+{-Task 3.4-}
 
 -- Returns an empty hand
 empty :: Hand
@@ -25,12 +25,27 @@ empty = Empty
 -- Calculates the value of a given hand, 
 -- where the value is the sum of all cards rank
 value :: Hand -> Integer
-value Empty = 0
-value (Add (Card (Numeric v) _) h) = v + value h
-value (Add (Card _ _) h) = 10 + value h
+value h = if v > 21 then v - 10 * a else v
+    where (v, a) = valueHelper h
 
+-- Helper function for value, keeping track of the number of aces in the hand
+valueHelper :: Hand -> (Integer, Integer)
+valueHelper Empty = (0, 0)
+valueHelper (Add (Card (Numeric n) _) h) = (v + n, a)
+    where (v, a) = valueHelper h
+valueHelper (Add (Card Ace _) h) =  (v + 11, a + 1)
+    where (v, a) = valueHelper h
+valueHelper (Add (Card _ _) h) = (v + 10, a)
+    where (v, a) = valueHelper h
+
+-- Tells if a hand is busted
 gameOver :: Hand -> Bool
-gameOver _ = undefined
+gameOver h | value h <= 21 = False
+gameOver _ = True
 
+-- Determine whose hand will win
 winner :: Hand -> Hand -> Player
-winner _ _ = undefined
+winner g _ | gameOver g = Bank
+winner _ b | gameOver b = Guest
+winner g b | value g > value b = Guest
+winner _ _ = Bank
