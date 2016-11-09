4d44e0b78eb9064240ea5c8f27416215e16534aa
Olle Svensson <olle@kent.nu>
2016-11-09 16:56:06 +0100

    Lab2 part B intermediate commit

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -16,12 +16,11 @@ size hand2
 
 {-Task 3.4-}
 
--- | Returns an empty hand
+-- | Returns an empty hand.
 empty :: Hand
 empty = Empty
 
--- | Returns the value of a given hand by summarizing the rank of all
--- | cards.
+-- | Returns the value of a given hand by summarizing the rank of all cards.
 value :: Hand -> Integer
 value h = if v > 21 then v - 10 * numberOfAces h else v
     where v = sumHand h
@@ -48,14 +47,52 @@ sumHand :: Hand -> Integer
 sumHand Empty = 0
 sumHand (Add c h) = valueCard c + sumHand h
 
--- Determines wether a hand has gone bust.
+-- | Determines wether a hand has gone bust.
 gameOver :: Hand -> Bool
 gameOver h | value h <= 21 = False
 gameOver _ = True
 
--- Determine whose hand will win.
+-- | Determine whose hand will win.
 winner :: Hand -> Hand -> Player
 winner g _ | gameOver g = Bank
 winner _ b | gameOver b = Guest
 winner g b | value g > value b = Guest
 winner _ _ = Bank
+
+hand1 = Add (Card (Numeric 5) Spades) Empty
+hand2 = Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty)
+hand3 = Add (Card Ace Hearts) (Add (Card (Numeric 4) Spades) (Add (Card Queen Diamonds) Empty))
+
+(<+) :: Hand -> Hand -> Hand
+(<+) (Add c h1) h2 = Add c (h1 <+ h2)
+(<+) Empty (Add c h2) = Add c (Empty <+ h2)
+(<+) _ Empty = Empty
+
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
+prop_onTopOf_assoc p1 p2 p3 =
+    p1<+(p2<+p3) == (p1<+p2)<+p3
+
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf h1 h2 = size (h1 <+ h2) == (size h1 + size h2)
+
+fullDeck :: Hand
+fullDeck = fullSuit Hearts <+ fullSuit Spades <+ 
+  fullSuit Diamonds <+ fullSuit Clubs
+
+fullSuit :: Suit -> Hand
+fullSuit s = buildSuit s Empty
+
+buildSuit :: Suit -> Hand -> Hand
+buildSuit _ h@(Add (Card Ace _) _) = h
+buildSuit s h@(Add (Card King _) _) = 
+  buildSuit s (Add (Card Ace s) h)
+buildSuit s h@(Add (Card Queen _) _) = 
+  buildSuit s (Add (Card King s) h)
+buildSuit s h@(Add (Card Jack _) _) = 
+  buildSuit s (Add (Card Queen s) h)
+buildSuit s h@(Add (Card (Numeric 10) _) _) = 
+  buildSuit s (Add (Card Jack s) h)
+buildSuit s h@(Add (Card (Numeric n) _) _) = 
+  buildSuit s (Add (Card (Numeric (n + 1)) s) h)
+buildSuit s Empty = 
+  buildSuit s (Add (Card (Numeric 2) s) Empty)
