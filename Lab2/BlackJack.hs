aa30f38631b111e913c00d493b81438050fe1eab
Olle Svensson <olle@kent.nu>
2016-11-22 19:44:17 +0100

    Updated Lab2 based on feedback from reject2

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -1,9 +1,9 @@
 module BlackJack where
-import Cards
-import RunGame
+import           Cards
+import           RunGame
 
-import System.Random
-import Test.QuickCheck hiding (shuffle)
+import           System.Random
+import           Test.QuickCheck hiding (shuffle)
 {-Task 3.1-}
 {-
 size hand2
@@ -67,12 +67,13 @@ hand1 = Add (Card (Numeric 5) Spades) Empty
 hand2 = Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty)
 hand3 = Add (Card Ace Hearts) (Add (Card (Numeric 4) Spades) (Add (Card Queen Diamonds) Empty))
 
--- | OnTopOf: Puts one hand ontop of another hand 
+-- | OnTopOf: Puts one hand ontop of another hand
 (<+) :: Hand -> Hand -> Hand
+(<+) h1 Empty = h1
 (<+) (Add c h1) h2 = Add c (h1 <+ h2)
 (<+) Empty h2 = h2
 
--- | Property: Associativity of operator (<+) 
+-- | Property: Associativity of operator (<+)
 prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
 prop_onTopOf_assoc p1 p2 p3 = p1<+(p2<+p3) == (p1<+p2)<+p3
 
@@ -82,24 +83,23 @@ prop_size_onTopOf h1 h2 = size (h1 <+ h2) == (size h1 + size h2)
 
 -- | Returns a full deck of 52 cards
 fullDeck :: Hand
-fullDeck = fullSuit Hearts <+ fullSuit Spades <+ 
+fullDeck = fullSuit Hearts <+ fullSuit Spades <+
            fullSuit Diamonds <+ fullSuit Clubs
 
 -- | Returns a hand of cards with all ranks of one suit
 fullSuit :: Suit -> Hand
 fullSuit suit = foldr Add Empty suitList
-    where suitList = [Card (Numeric rank) suit | rank <- [2..10]] ++
-                     [Card Jack suit, 
-                      Card Queen suit, 
-                      Card King suit, 
-                      Card Ace suit]
+    where suitList = [Card rank suit | rank <- ranks]
+          ranks = numerics ++ courts
+          numerics = [Numeric n | n <- [2..10]]
+          courts = [Jack, Queen, King, Ace]
 
 -- | Moves the top card of a hand to another hand
 draw :: Hand -> Hand -> (Hand,Hand)
 draw Empty _h    = error "draw: The deck is empty."
-draw (Add c d) h = (d, Add c h) 
+draw (Add c d) h = (d, Add c h)
 
--- | Draws cards from a deck until the value of the hand exceeds 16 
+-- | Draws cards from a deck until the value of the hand exceeds 16
 playBank :: Hand -> Hand
 playBank deck = bankDraw (deck, Empty)
   where bankDraw (d, h) | value h >= 16 = h
@@ -108,23 +108,20 @@ playBank deck = bankDraw (deck, Empty)
 -- | Shuffles a deck
 shuffle :: StdGen -> Hand -> Hand
 shuffle g d = shuffler g (d, Empty) (size d)
+  where shuffler g (d1, d2) 0 = d2
+        shuffler g (d1, d2) s = shuffler g' (d1', Add c d2) (s-1)
+          where (n, g') = randomR (1, s) g
+                (c, d1') = removeNth d1 n
 
--- | Helper function for shuffle
-shuffler :: StdGen -> (Hand, Hand) -> Integer -> Hand
-shuffler g (d1, d2) 0 = d2
-shuffler g (d1, d2) s = shuffler g' (d1', Add c d2) (s-1)
-  where (n, g') = randomR (1, s) g
-        (c, d1') = removeNth d1 n
-
--- | Removes the Nth card in a hand and returns the removed card 
+-- | Removes the Nth card in a hand and returns the removed card
 -- | and the remaining hand
 removeNth :: Hand -> Integer -> (Card, Hand)
-removeNth Empty _ = error "removeNth: no deck" 
+removeNth Empty _ = error "removeNth: no deck"
 removeNth d n = remove (d, Empty) n
   where remove (h1@(Add c h), h2) i | i <=1     = (c, h <+ h2)
                                     | otherwise = remove (draw h1 h2) (i-1)
 
--- | Property: The shuffled deck contains the same cards as the 
+-- | Property: The shuffled deck contains the same cards as the
 -- | unshuffled deck
 prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
 prop_shuffle_sameCards g c h =
@@ -146,7 +143,7 @@ implementation = Interface
   , iFullDeck = fullDeck
   , iValue    = value
   , iGameOver = gameOver
-  , iWinner   = winner 
+  , iWinner   = winner
   , iDraw     = draw
   , iPlayBank = playBank
   , iShuffle  = shuffle
